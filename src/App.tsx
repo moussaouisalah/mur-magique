188d39c7286cbd51f34b7272aa3ef47006aba3bb
Salah Moussaoui <60707540+moussaouisalah@users.noreply.github.com>
2022-07-16 14:22:22 +0100

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import List from "./pages/List";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Upload from "./pages/Upload";
+
 function App() {
-  return <div className="flex justify-center bg-red-500">test</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="upload" element={<Upload />} />
+      <Route path="list" element={<List />} />
+    </Routes>
+  );
 }
 
 export default App;
